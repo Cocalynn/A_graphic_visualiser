2505b9e233c7c9ba44872a8253f4226a57c9746a
Cocalynn <yinong.li97@gmail.com>
2021-08-31 18:28:36 +0100

    encountering bug where it showed algorithm module is not callabl

--- Astar.py
+++ Astar.py
@@ -1,9 +1,9 @@
+import algorithm as algorithm
 import pygame
 import math
 from queue import PriorityQueue
 
 # pygame display setting
-
 WIDTH = 800  # width of the window, if it is 50*50, then each grid width will be 800/50
 WIN = pygame.display.set_mode((WIDTH, WIDTH))  # how big the display is going to be
 pygame.display.set_caption("A* Path Finding Algorithm")
@@ -77,7 +77,20 @@ class Spot:
         # rect means the way to draw a rectangle (location and size)
 
     def update_neighbours(self, grid):
-        pass
+        self.neighbours = []
+
+        # if this row is the
+        if self.row < self.total_rows - 1 and not grid[self.row + 1][self.col].is_barrier():  # down
+            self.neighbours.append(grid[self.row + 1][self.col])
+
+        if self.row > 0 and not grid[self.row - 1][self.col].is_barrier():  # up
+            self.neighbours.append(grid[self.row - 1][self.col])
+
+        if self.col < self.total_rows - 1 and not grid[self.row][self.col + 1].is_barrier():  # right
+            self.neighbours.append(grid[self.row][self.col + 1])
+
+        if self.col > 0 and not grid[self.row][self.col - 1].is_barrier():  # left
+            self.neighbours.append(grid[self.row][self.col - 1])
 
     def __lt__(self, other):  # lt means less than, we compare two spots here
         return False
@@ -90,6 +103,48 @@ def h(p1, p2):  # manhattan distance
     x1, y1 = p1  # coordinate of p1  eg.p1=(1,9)
     x2, y2 = p2
     return abs(x1 - x2) + abs(y1 - y2)
+
+
+def algorith(draw, grid, start, end):
+    count = 0
+    open_set = PriorityQueue()
+    open_set.put((0, count, start))
+    came_from = {}
+    g_score = {spot: float("inf") for row in grid for spot in row}
+    g_score[start] = 0
+    f_score = {spot: float("inf") for row in grid for spot in row}
+    f_score[start] = h(start.get_pos(), end.get_pos())
+
+    open_set_hash = {start}
+
+    while not open_set.empty():
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
+        current = open_set.get()[2]
+        open_set_hash.remove(current)
+
+        if current == end:
+            return True
+
+        for neighbor in current.neighbours:
+            temp_g_score = g_score[current] + 1
+
+            if temp_g_score < g_score[neighbor]:
+                came_from[neighbor] = current
+                g_score[neighbor] = temp_g_score
+                f_score[neighbor] = temp_g_score + h(neighbor.get_pos(), end.get_pos())
+                if neighbor not in open_set_hash:
+                    count += 1
+                    open_set.put(f_score[neighbor], count, neighbor)
+                    open_set_hash.add(neighbor)
+                    neighbor.make_open()
+        draw()
+
+        if current != start:
+            current.make_closed()
+    return False
 
 
 # make a grid
@@ -113,7 +168,7 @@ def draw_grid(win, rows, width):
             pygame.draw.line(win, GREY, (j * gap, 0), (j * gap, width))  # vertical lines
 
 
-# draw everything
+# draw everything including grid lines
 def draw(win, grid, rows, width):
     win.fill(WHITE)  # fills the screen with white color
 
@@ -149,40 +204,45 @@ def main(win, width):
 
     while run:
         draw(win, grid, ROWS, width)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+        for event in pygame.event.get():  # to monitor the user event
+            if event.type == pygame.QUIT:  # if we press the X button at the top right hand corner, then stop running the game and quit
                 run = False
 
             if started:
                 continue
 
-            if pygame.mouse.get_pressed()[0]:  # mouse left
-                pos = pygame.mouse.get_pos()
-                row, col = get_clicked_pos(pos, ROWS, width)
+            if pygame.mouse.get_pressed()[0]:  # if the user click the left mouse; 0 1 2: left middle right
+                pos = pygame.mouse.get_pos()  # get the mouse click position
+                row, col = get_clicked_pos(pos, ROWS, width)  # get mouse row and col
                 spot = grid[row][col]
-                if not start and spot != end: # start spot and end spot are not the same spot
-                    start = spot
-                    start.make_start()
-                elif not end and spot != end: # start spot and end spot are not the same spot
-                    end = spot
-                    end.make_end()
-                elif spot != end and spot != start:
+                if not start and spot != end:  # start spot and end spot are not the same spot
+                    start = spot  # make the mouse spot as the start
+                    start.make_start()  # make the color as orange
+                elif not end and spot != end:  # end spot and end spot are not the same spot
+                    end = spot  # make the mouse spot as the end
+                    end.make_end()  # make the color as turquoise
+                elif spot != end and spot != start:  # barrier spot
                     spot.make_barrier()
-            elif pygame.mouse.get_pressed()[2]:  # mouse right
-                pos = pygame.mouse.get_pos()
-                row, col = get_clicked_pos(pos, ROWS, width)
+            elif pygame.mouse.get_pressed()[2]:  # if the user click the right mouse
+                pos = pygame.mouse.get_pos()  # get the mouse click position
+                row, col = get_clicked_pos(pos, ROWS, width)  # get mouse row and col
                 spot = grid[row][col]
-                spot.reset()
+                spot.reset()  # make it white
                 if spot == start:
-                    start = None
+                    start = None  # reset the start
 
                 elif spot == end:
-                    end = None
+                    end = None  # reset the end
 
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE and not started:
-                    pass
-    pygame.quit()
+            if event.type == pygame.KEYDOWN:  # if the keyboard is pressed
+                if event.key == pygame.K_SPACE and not started:  # if the key is the space bar and we haven't started the algorithm
+                    for row in grid:
+                        for spot in row:
+                            spot.update_neighbours(grid)
+
+                    algorithm(lambda: draw(win, grid, ROWS, width), grid, start, end)
+
+    pygame.quit()  # exit the game window
 
 
 main(WIN, WIDTH)
